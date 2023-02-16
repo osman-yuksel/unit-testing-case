9fca57dbfed211b6b7195e4837e4ed0055ffc0eb
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-02-16 18:57:41 +0300

    feat: function returns empty array if argument types are valid

---
+++ src/utils/fibonacci.ts
@@ -0,0 +1,6 @@
+export default function Fibonacci(start: number, end: number) {
+  if (typeof start !== "number" || typeof end !== "number") {
+    throw new Error("function paramaters must be number!");
+  }
+  return [];
+}
