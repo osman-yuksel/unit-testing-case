1ed5f2cb8e157c38636a5988a60ffb92d28eb91b
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-02-16 22:40:15 +0300

    feat: fibonacci sequence calculation logic

--- src/utils/fibonacci.ts
+++ src/utils/fibonacci.ts
@@ -2,5 +2,17 @@ export default function Fibonacci(start: number, end: number) {
   if (typeof start !== "number" || typeof end !== "number") {
     throw new Error("function paramaters must be number!");
   }
-  return [];
+
+  const ans: number[] = [];
+  let n1 = 1,
+    n2 = 1,
+    nextN: number;
+
+  for (let i = 1; i <= end; i++) {
+    ans.push(n1);
+    nextN = n1 + n2;
+    n1 = n2;
+    n2 = nextN;
+  }
+  return ans.slice(start - 1);
 }
