748278b1568d9ea5c169c8aa1d47112d296891ea
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-02-16 22:40:44 +0300

    feat: new errors for various cases

--- src/utils/fibonacci.ts
+++ src/utils/fibonacci.ts
@@ -1,6 +1,22 @@
 export default function Fibonacci(start: number, end: number) {
   if (typeof start !== "number" || typeof end !== "number") {
-    throw new Error("function paramaters must be number!");
+    throw new Error("function parameters must be number!");
+  }
+
+  if (start === Infinity || end === Infinity) {
+    throw new Error("function parameters can't be Infinity!");
+  }
+
+  if (end > 1476) {
+    console.warn("numbers after the 1476th will be Infinity!");
+  }
+
+  if (start < 1 || end < 1) {
+    throw new Error("function parameters must be greater than 0!");
+  }
+
+  if (start - end > 0) {
+    throw new Error("'end' can't be lower than 'start'!");
   }
 
   const ans: number[] = [];
