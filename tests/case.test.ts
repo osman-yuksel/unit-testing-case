c778ce57b92095dafe3468eb69db161a7a799387
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-02-16 22:54:22 +0300

    test: functionality and parameter validation

--- tests/case.test.ts
+++ tests/case.test.ts
@@ -1,17 +1,50 @@
-//@ts-nocheck
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import Fibonacci from "../src/utils/fibonacci";
 
-describe("fibonacci", () => {
-  test("initial test, returns an empty array", () => {
-    expect(Fibonacci(0, 0)).eql([]);
+describe("fibonacci - parameter validation", () => {
+  test("paramater types", () => {
+    // @ts-expect-error
+    expect(() => Fibonacci("abc", 0)).toThrowError("function parameters must be number!");
+    // @ts-expect-error
+    expect(() => Fibonacci(0, "abc")).toThrowError("function parameters must be number!");
   });
 
-  test("'start' paramater type check", () => {
-    expect(() => Fibonacci("abc", 0)).toThrowError();
+  test("positive parameter", () => {
+    expect(() => Fibonacci(4, 0)).toThrowError("function parameters must be greater than 0!");
+    expect(() => Fibonacci(-2, 11)).toThrowError("function parameters must be greater than 0!");
   });
 
-  test("'end' paramater type check 1", () => {
-    expect(() => Fibonacci(0, "abc")).toThrowError();
+  test("Infinity number", () => {
+    expect(() => Fibonacci(Infinity, 12)).toThrowError("function parameters can't be Infinity!");
+    expect(() => Fibonacci(4, Infinity)).toThrowError("function parameters can't be Infinity!");
+  });
+
+  test("checks if 'end' >= 'start'?", () => {
+    expect(Fibonacci(4, 4)).toBeTruthy();
+    expect(() => Fibonacci(5, 2)).toThrowError("'end' can't be lower than 'start'!");
+  });
+});
+
+describe("fibonacci - functionality", () => {
+  test("initial test, returns [1]", () => {
+    expect(Fibonacci(1, 1)).eql([1]);
+  });
+
+  test("calculation test 1", () => {
+    expect(Fibonacci(3, 5)).eql([2, 3, 5]);
+  });
+
+  test("calculation test 2", () => {
+    expect(Fibonacci(1, 10)).eql([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+  });
+
+  test("calculation test 3", () => {
+    expect(Fibonacci(500, 500)).eql([1.394232245616977e104]);
+  });
+
+  test("Number.MAX_VALUE", () => {
+    const spy = vi.spyOn(console, "warn");
+    Fibonacci(2, 1477);
+    expect(spy).toHaveBeenCalledWith("numbers after the 1476th will be Infinity!");
   });
 });
